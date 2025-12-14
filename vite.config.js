b90cd5959efaba616df67a5a3aeab95ed9f8ad3f
kentlee406 <157895655+kentlee406@users.noreply.github.com>
2025-12-14 20:31:17 +0800

    init 6

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/js_react_practice/" : "/",
+  base: "/js_react_practice/",
   plugins: [
     react({
       babel: {
